df5446e3a6319408728723b1eb997b93b4c28dcb
Ryn's <39035843+RynKings@users.noreply.github.com>
2019-05-23 11:52:29 +0800

    Update __init__.py

--- instagrampy/__init__.py
+++ instagrampy/__init__.py
@@ -3,7 +3,7 @@ from .instaclient import INSTAGRAM
 __copyright__       = 'Copyright 2019 by Ryns'
 __version__         = '1.0'
 __author__          = 'Ryns'
-__author_email__    = 'ryns@ryns.site'
+__author_email__    = 'ryns@chocola.dev'
 __url__             = 'http://github.com/rynkings/instagram-py'
 
 __all__ = ['INSTAGRAM', '__author__']
